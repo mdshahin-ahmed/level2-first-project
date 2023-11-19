957da4b64996861588b4aeeaa6ca5a81e6f75bdb
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2023-11-19 20:23:23 +0600

    Initial setup done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
-import express, { Request, Response } from "express";
-const app = express();
-const port = 3000;
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+// parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+
+  res.send(a);
 });
+
+export default app;
