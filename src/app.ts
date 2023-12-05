042302aeb6f2dd7a28ae0b442c6c6887dc9fb305
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2023-12-05 17:39:04 +0600

    handle : unHandleRejection nad unCaughtException

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
+import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
@@ -17,13 +15,13 @@ app.use(cors());
 // /api/v1/students
 app.use('/api/v1', router);
 
-const getAController = (req: Request, res: Response) => {
-  const a = 10;
+const test = (req: Request, res: Response) => {
+  const a = 200;
 
   res.send(a);
 };
 
-app.get('/', getAController);
+app.get('/', test);
 
 app.use(globalErrorHandler);
 
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
+import { Server } from 'http';
 
-// main().catch(err => console.log(err));
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example apps listening on port ${config.port}`);
     });
   } catch (error) {
@@ -17,3 +18,19 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log(`😡 unhandledRejection is detected, Shutting down...`);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😡 uncaughtException is detected, Shutting down...`);
+  process.exit(1);
+});
