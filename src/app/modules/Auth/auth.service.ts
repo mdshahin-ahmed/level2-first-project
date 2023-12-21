10093622d3ce683a4c5469dce4e70aa99c39dd08
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2023-12-21 16:35:13 +0600

    Checking id and password

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,8 +1,36 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/app.error';
+import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
+import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: TLoginUser) => {
-  console.log(payload);
+  // checking is the user is exist
+  const isUserExists = await User.findOne({ id: payload?.id });
 
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
+  }
+
+  // checking if the user is already deleted
+  const isDeleted = isUserExists?.isDeleted;
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted');
+  }
+  // checking if the user is blocked
+  const userStatus = isUserExists?.status;
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked!');
+  }
+
+  // checking if the password is correct
+  const isPasswordMatched = await bcrypt.compare(
+    payload.password,
+    isUserExists?.password,
+  );
+  console.log(isPasswordMatched);
+
+  // access granted -> send access token -> refresh token
   return {};
 };
 
