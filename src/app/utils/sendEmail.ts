cfb4e8ee99cbbac35e04bd7d0b4fcd401a827880
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2023-12-23 22:18:30 +0600

    Successfully send link

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -1,22 +1,23 @@
 import nodemailer from 'nodemailer';
+import config from '../config';
 
-export const sendEmail = async () => {
+export const sendEmail = async (to: string, html: string) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.forwardemail.net',
-    port: 465,
-    secure: true,
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: config.NODE_ENV === 'production',
     auth: {
       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: 'REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM',
-      pass: 'REPLACE-WITH-YOUR-GENERATED-PASSWORD',
+      user: 'mdshahinahmed605702@gmail.com',
+      pass: 'lsjw hvqz disv kark',
     },
   });
 
   await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: 'bar@example.com, baz@example.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Hello world?</b>', // html body
+    from: 'mdshahinahmed605702@gmail.com', // sender address
+    to, // list of receivers
+    subject: 'Change password!', // Subject line
+    text: 'Reset your password within 10 minutes', // plain text body
+    html, // html body
   });
 };
