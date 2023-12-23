1a786133b25e7e066ac930fb458d544a7ff9c9d4
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2023-12-23 11:25:34 +0600

    Expired token after change password

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -13,10 +13,16 @@ export interface TUser {
 
 export interface UserModel extends Model<TUser> {
   isUserExistsByCustomId(id: string): Promise<TUser>;
+
   isPasswordMatched(
     plainTextPassword: string,
     hashedPassword: string,
   ): Promise<boolean>;
+
+  isJWTIssuedBeforePasswordChanged(
+    passwordChangedTimestamp: Date,
+    jwtIssuedTimestamp: number,
+  ): boolean;
 }
 
 export type TUserRole = keyof typeof USER_ROLE;
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -72,4 +72,14 @@ userSchema.statics.isPasswordMatched = async function (
   return await bcrypt.compare(plainTextPassword, hashedPassword);
 };
 
+userSchema.statics.isJWTIssuedBeforePasswordChanged = function (
+  passwordChangedTimestamp: Date,
+  jwtIssuedTimestamp: number,
+) {
+  const passwordChangedTime =
+    new Date(passwordChangedTimestamp).getTime() / 1000;
+
+  return passwordChangedTime > jwtIssuedTimestamp;
+};
+
 export const User = model<TUser, UserModel>('User', userSchema);
