b88f6b4e075b41817d5bf307c5270158e0830372
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2023-12-25 20:21:55 +0600

    successfully upload image on cloudinary and save url into DB

--- src/app/utils/sendImageToCloudinary.ts
+++ src/app/utils/sendImageToCloudinary.ts
@@ -2,6 +2,8 @@ import { v2 as cloudinary } from 'cloudinary';
 import config from '../config';
 import multer from 'multer';
 
+import fs from 'fs';
+
 cloudinary.config({
   cloud_name: config.cloudinary_cloud_name,
   api_key: config.cloudinary_api_key,
@@ -18,6 +20,14 @@ export const sendImageToCloudinary = (imageName: string, path: string) => {
           reject(error);
         }
         resolved(result);
+        // delete a file asynchronously
+        fs.unlink(path, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('File is deleted!');
+          }
+        });
       },
     );
   });
